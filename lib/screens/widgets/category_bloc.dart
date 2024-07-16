2790b204e69de29e3037961a7c617db719de70ea
izhar <izharnjoroge@gmail.com>
2024-07-16 12:28:43 +0300

    fixed horizontal scroll loading

--- lib/screens/widgets/category_bloc.dart
+++ lib/screens/widgets/category_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerceapp/blocs/categoryBloc/category_cubit.dart';
+import 'package:ecommerceapp/models/category_model.dart';
 import 'package:ecommerceapp/screens/widgets/items_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,13 +26,34 @@ class CategoryContent extends StatefulWidget {
   State<CategoryContent> createState() => _CategoryContentState();
 }
 
-class _CategoryContentState extends State<CategoryContent> {
+class _CategoryContentState extends State<CategoryContent>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  bool _isTabControllerInitialized = false;
   String selectedID = '';
 
   @override
   void initState() {
     super.initState();
     context.read<CategoryCubit>().getCategories();
+  }
+
+  @override
+  void dispose() {
+    if (_isTabControllerInitialized) {
+      _tabController.dispose();
+    }
+    super.dispose();
+  }
+
+  void _initializeTabController(List<CategoryModel> categories) {
+    _tabController = TabController(length: categories.length, vsync: this);
+    _tabController.addListener(() {
+      setState(() {
+        selectedID = categories[_tabController.index].category_id;
+      });
+    });
+    _isTabControllerInitialized = true;
   }
 
   @override
@@ -43,11 +65,17 @@ class _CategoryContentState extends State<CategoryContent> {
             child: CircularProgressIndicator(color: Colors.purple),
           );
         } else if (state is CategoryLoaded) {
+          // Initialize the TabController if it's not already initialized
+          if (!_isTabControllerInitialized) {
+            _initializeTabController(state.categoryModel);
+          }
+
           return DefaultTabController(
             length: state.categoryModel.length,
             child: Column(
               children: [
                 TabBar(
+                  controller: _tabController,
                   onTap: (value) {
                     setState(() {
                       selectedID = state.categoryModel[value].category_id;
@@ -81,6 +109,7 @@ class _CategoryContentState extends State<CategoryContent> {
                   child: Padding(
                     padding: const EdgeInsets.only(top: 10),
                     child: TabBarView(
+                      controller: _tabController,
                       children: state.categoryModel.map((category) {
                         return ItemsBloc(
                           categoryId: selectedID == ''
@@ -100,7 +129,7 @@ class _CategoryContentState extends State<CategoryContent> {
           );
         } else {
           return const Center(
-            child: Text('error fetching data'),
+            child: Text('Error fetching data'),
           );
         }
       },
